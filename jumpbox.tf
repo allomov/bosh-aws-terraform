d2d68e9aa6de25979315ceff8194d2b15babf09b
A.S. Lomoff <lomov.as@gmail.com>
2016-11-23 17:39:45 +0300

    add new networking and outputs

---
+++ jumpbox.tf
@@ -0,0 +1,50 @@
+resource "aws_instance" "jumpbox" {
+    ami = "${lookup(var.amis, var.aws_region)}"
+    instance_type = "t2.micro"
+    key_name = "${aws_key_pair.jumpbox.key_name}"
+    security_groups = [
+        "${aws_security_group.ssh_only.id}"
+    ]
+    subnet_id = "${aws_subnet.vpc_public_subnet.id}"
+    associate_public_ip_address = true
+    source_dest_check = false
+
+    tags {
+        Name = "${var.prefix}_jumpbox"
+    }
+
+    connection {
+      type = "ssh"
+      user = "${var.jumpbox_user}"
+      private_key = "${file("${var.aws_private_key}")}"
+      timeout = "5m"
+    }
+
+    provisioner "file" {
+        source = "${var.aws_private_key}"
+        destination = "/home/ubuntu/.ssh/id_rsa"
+    }
+
+    provisioner "file" {
+        source = "${var.aws_public_key}"
+        destination = "/home/ubuntu/.ssh/id_rsa.pub"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo apt-get update",
+            "sudo apt-get install software-properties-common -y",
+            "sudo apt-add-repository ppa:ansible/ansible -y",
+            "sudo apt-get update",
+            "sudo apt-get install ansible -y",
+            "sudo ansible-galaxy install allomov.bosh-jumpbox",
+            "ansible-playbook -i /etc/ansible/roles/allomov.bosh-jumpbox/examples/localhost/hosts /etc/ansible/roles/allomov.bosh-jumpbox/examples/localhost/playbook.yml"
+        ]
+    }
+
+}
+
+resource "aws_key_pair" "jumpbox" {
+  key_name = "${var.prefix}_jumpbox_key"
+  public_key = "${file("${var.aws_public_key}")}"
+}
--- variables.tf
+++ variables.tf
@@ -1,7 +1,8 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "aws_key_path" {}
-variable "aws_key_name" {}
+variable "aws_public_key" {}
+variable "aws_private_key" {}
+
 
 variable "prefix" {
   description = "A prefix to easily identify the resources in AWS"
@@ -19,19 +20,19 @@ variable "aws_az" {
 }
 
 variable "amis" {
-    description = "AMIs by region"
+    description = "Ubuntu 16.04 LTS AMIs by region"
     default = {
-        us-east-1      = "ami-2d39803a"
-        us-west-2      = "ami-42569022"
-        us-west-1      = "ami-992661f9"
-        eu-west-1      = "ami-02b62c71"
-        eu-central-1   = "ami-4bd03b24"
-        ap-southeast-1 = "ami-ea2bf989"
-        ap-northeast-1 = "ami-2e63924f"
-        ap-southeast-2 = "ami-396a415a"
-        ap-northeast-2 = "ami-979258f9"
-        ap-south-1     = "ami-4a90fa25"
-        sa-east-1      = "ami-d952c7b5"
+        us-east-1 = "ami-45b69e52"
+        us-west-2 = "ami-9ee24ffe"
+        us-west-1 = "ami-dab5e0ba"
+        eu-west-1 = "ami-07174474"
+        eu-central-1 = "ami-82cf0aed"
+        ap-southeast-1 = "ami-5b268538"
+        ap-northeast-1 = "ami-483f9429"
+        ap-southeast-2 = "ami-e199a682"
+        ap-northeast-2 = "ami-27825549"
+        ap-south-1 = "ami-dfd1a5b0"
+        sa-east-1 = "ami-0c188760"
     }
 }
 
